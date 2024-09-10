89bd144c1519ab61c53c21e5146f7738e3a3105a
kauanmagaldi29 <kauanmagaldi29@gmail.com>
2024-09-10 19:59:39 -0300

    asda

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -13,7 +13,7 @@ const Testimonials = () => {
           <img src={testimonialPerson} alt="testimonial Person" className="testimonialsBoxImg" />
         </div>
         <p className="testimonialsDesc font-bold text-[32px] text-center max-lg:text-[25px] max-md:text-[20px]">
-          "Sentia muita dor na boca, além de meu sorriso ter várias imporfeições, mas depois da consulta com a Dra. Anna Chiodelli. Estou com o meu sorriso novo, coloquei lentes além de fazer remoção de cáries."
+          "Sentia muita dor na boca, além de meu sorriso ter várias imporfeições, mas depois da consulta com a Dra. Anna Chiodelli. Estou com o meu sorriso novo, fiz remoção de cáries e clareamento.""
         </p>
         <div className="testimonialsBoxPersonInfo text-center mt-5">
           <p className="testimonialsBoxPersonName text-[24px] font-medium text-[#1E1E1E]">David Gahan</p>
